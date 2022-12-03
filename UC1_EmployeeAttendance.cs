5569dfd983691ad02c8ad8e8b4e8def5d3144506
“akashmane3938” <maneakash3938@gmail.com>
2022-12-03 00:46:25 -0800

    Check Employee Present OR Absent

---
+++ UC1_EmployeeAttendance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    class UC1_EmployeeAttendance
+    {
+        public static void EmployeeAttendances()
+        {
+            //Constant
+            int ISFULLTIME = 0;
+            Random random = new Random();//Create A Object
+            int empcheck=random.Next(0,2);
+            if(empcheck==0)//Checking Condition 
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+            }
+        }
+    }
+}
+ 
